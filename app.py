db75d8b5444ff22223a31a0ba2fae97a0108d976
Naoto MINAMI <minami@minami.local>
2017-08-06 00:00:51 +0900

    add: carousel

--- app.py
+++ app.py
@@ -34,6 +34,55 @@ def get_reply_text():
     ]
     return texts[randint(0, len(texts)-1)]
 
+def get_carousel_message():
+    return TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item1.jpg',
+                    title='this is menu1',
+                    text='description1',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='postback1',
+                            text='postback text1',
+                            data='action=buy&itemid=1'
+                        ),
+                        MessageTemplateAction(
+                            label='message1',
+                            text='message text1'
+                        ),
+                        URITemplateAction(
+                            label='uri1',
+                            uri='http://example.com/1'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://example.com/item2.jpg',
+                    title='this is menu2',
+                    text='description2',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='postback2',
+                            text='postback text2',
+                            data='action=buy&itemid=2'
+                        ),
+                        MessageTemplateAction(
+                            label='message2',
+                            text='message text2'
+                        ),
+                        URITemplateAction(
+                            label='uri2',
+                            uri='http://example.com/2'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+
 app = Flask(__name__)
 channel_access_token = os.environ['channel_access_token']
 channel_access_secret = os.environ['channel_access_secret']
@@ -55,13 +104,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    reply_text = '長すギィ...' if len(event.message.text) > 140 else get_reply_text()
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_text)
-    )
-    twitter_bot_api.update_status(event.message.text.replace('@', ''))
+    if event.message.text == 'carousel':
+        message = get_carousel_message()
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
+    else:
+        reply_text = '長すギィ...' if len(event.message.text) > 140 else get_reply_text()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_text)
+        )
+        twitter_bot_api.update_status(event.message.text.replace('@', ''))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
